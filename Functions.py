e15d877e327311e8a7f5c1e66c898b0a16583971
jxie10 <jxie10@stevens.edu>
2019-04-08 00:11:59 -0400

    Function modified

---
+++ Functions.py
@@ -0,0 +1,122 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+import random
+pool=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+player_handcard=[]
+ai_handcard=[]
+player_score = 0
+ai_score = 0
+
+def Dealing(pool):
+    #release a poker and delete the card in the pool
+    return pool.pop(random.randint(0,len(pool)-1))
+
+def Conditional_Dealing(pool):
+    #Make sure the initial cards are not larger than 21
+    player_handcard.append(Dealing(pool))
+    ai_handcard.append(Dealing(pool))
+    while player_handcard[0]+player_handcard[1]>21:
+        pool.append(player_handcard[1])
+        del player_handcard[1]
+        player_handcard.append(Dealing(pool))
+    while ai_handcard[0]+ai_handcard[1]>21:
+        pool.append(ai_handcard[1])
+        del ai_handcard[1]
+        ai_handcard.append(Dealing(pool))
+    return player_handcard,ai_handcard
+
+def Hit_or_Stand():
+    if player_score >= 21:
+        pygame.event.set_blocked(MOUSEBUTTONDOWN)
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        player_handcard.append(Dealing(pool))
+        return player_handcard
+
+def judgement(player_score,ai_socre):
+    Win,Draw,Loose = 1,2,3
+    if player_score > 21 and ai_socre > 21:
+        return Draw
+    elif player_score == 21 and ai_socre == 21:
+        return Draw
+    elif player_score > 21 and ai_socre <= 21:
+        return Loose
+    elif player_score <= 21 and ai_socre > 21:
+        return Win
+    elif player_score > ai_socre:
+        return Win
+    elif player_score == ai_socre:
+        return Draw
+    else:
+        return Loose
+
+
+def Drawcard_player(cardnum,pos1,pos2):
+    cardnum -= 1
+    if player_handcard[cardnum] == 1:
+        screen.blit(card1,(pos1,pos2))
+    elif player_handcard[cardnum] == 2:
+        screen.blit(card2,(pos1,pos2))
+    elif player_handcard[cardnum] == 3:
+        screen.blit(card3,(pos1,pos2))
+    elif player_handcard[cardnum] == 4:
+        screen.blit(card4,(pos1,pos2))
+    elif player_handcard[cardnum] == 5:
+        screen.blit(card5,(pos1,pos2))
+    elif player_handcard[cardnum] == 6:
+        screen.blit(card6,(pos1,pos2))
+    elif player_handcard[cardnum] == 7:
+        screen.blit(card7,(pos1,pos2))
+    elif player_handcard[cardnum] == 8:
+        screen.blit(card8,(pos1,pos2))
+    elif player_handcard[cardnum] == 9:
+        screen.blit(card9,(pos1,pos2))
+    elif player_handcard[cardnum] == 10:
+        screen.blit(card10,(pos1,pos2))
+    elif player_handcard[cardnum] == 11:
+        screen.blit(card11,(pos1,pos2))
+    elif player_handcard[cardnum] == 12:
+        screen.blit(card12,(pos1,pos2))
+    elif player_handcard[cardnum] == 13:
+        screen.blit(card13,(pos1,pos2))
+
+
+
+def Drawcard_ai(cardnum,pos1,pos2):
+    cardnum -= 1
+    if ai_hide[cardnum] == 1:
+        screen.blit(card1,(pos1,pos2))
+    elif ai_hide[cardnum] == 2:
+        screen.blit(card2,(pos1,pos2))
+    elif ai_hide[cardnum] == 3:
+        screen.blit(card3,(pos1,pos2))
+    elif ai_hide[cardnum] == 4:
+        screen.blit(card4,(pos1,pos2))
+    elif ai_hide[cardnum] == 5:
+        screen.blit(card5,(pos1,pos2))
+    elif ai_hide[cardnum] == 6:
+        screen.blit(card6,(pos1,pos2))
+    elif ai_hide[cardnum] == 7:
+        screen.blit(card7,(pos1,pos2))
+    elif ai_hide[cardnum] == 8:
+        screen.blit(card8,(pos1,pos2))
+    elif ai_hide[cardnum] == 9:
+        screen.blit(card9,(pos1,pos2))
+    elif ai_hide[cardnum] == 10:
+        screen.blit(card10,(pos1,pos2))
+    elif ai_hide[cardnum] == 11:
+        screen.blit(card11,(pos1,pos2))
+    elif ai_hide[cardnum] == 12:
+        screen.blit(card12,(pos1,pos2))
+    elif ai_hide[cardnum] == 13:
+        screen.blit(card13,(pos1,pos2))
+    elif ai_hide[cardnum] == 'cardback':
+        screen.blit(cardback,(pos1,pos2))
+
+def Drawcard_player_else(initialposition1,initialposition2):
+    try:
+        for i in range(4):
+            i += 3
+            Drawcard_player(i,initialposition1,initialposition2)
+            initialposition1 += 132
+    except:pygame.display.update()
